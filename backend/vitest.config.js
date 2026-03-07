8423c80c3a622eca90beefd42953040fae6565f4
hetoke <hetokere32@gmail.com>
2026-03-07 19:30:53 +0700

    test: change vitest config

--- backend/vitest.config.js
+++ backend/vitest.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from "vitest/config";
-
 export default defineConfig({
   test: {
     environment: "node",
     globals: true,
     include: ["src/__tests__/**/*.test.js"],
-    setupFiles: ["./src/__tests__/setup/setupTestDB.js"]
+    setupFiles: ["./src/__tests__/setup/setupTestDB.js"],
+    fileParallelism: false
   }
 });
