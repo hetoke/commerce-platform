b26fe8152685b5f1df82307c49351b0b1b78c464
hetoke <hetokere32@gmail.com>
2026-03-07 19:47:44 +0700

    test: debug to find .env for CI

--- backend/src/__tests__/setup/setupTestDB.js
+++ backend/src/__tests__/setup/setupTestDB.js
@@ -1,12 +1,3 @@
-// setupTestDB.js
-import { fileURLToPath } from "url";
-import path from "path";
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-console.log("SETUP DIR:", __dirname);
-console.log("ENV PATH:", path.resolve(__dirname, "../../../.env"));
-console.log("CWD:", process.cwd());
-
 import { beforeAll, afterAll } from "vitest";
 import mongoose from "mongoose";
 import { MongoMemoryServer } from "mongodb-memory-server";
@@ -16,7 +7,11 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-dotenv.config({ path: path.resolve(__dirname, "../../../.env") }); // points to backend/.env
+console.log("SETUP DIR:", __dirname);
+console.log("ENV PATH:", path.resolve(__dirname, "../../../.env"));
+console.log("CWD:", process.cwd());
+
+dotenv.config({ path: path.resolve(__dirname, "../../../.env") });
 
 let mongo;
 beforeAll(async () => {
